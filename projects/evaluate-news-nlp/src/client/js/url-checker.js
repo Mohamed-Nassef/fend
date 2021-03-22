7d59f2f1e4be6021388c7889ec51f4e5ee0adb65
mohamed nassef <mohamadnassef419@gmail.com>
2021-03-22 22:22:50 +0200

    url cheaker

---
+++ projects/evaluate-news-nlp/src/client/js/url-checker.js
@@ -0,0 +1,9 @@
+function checkForUrl(url) {
+    const regex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gm;
+    const str = `http://www.grammarly.com/blog/category/product/`;
+
+
+    return regex.test(url)
+}
+
+export { checkForUrl }
